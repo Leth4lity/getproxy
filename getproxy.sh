bf9018ba5e80724e8e15579fd2b058246f1ba0b5
Laken J Brion <60785066+Leth4lity@users.noreply.github.com>
2022-04-29 17:35:37 -0500

    Add files via upload

---
+++ getproxy.sh
@@ -0,0 +1,4 @@
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt -o socks5.txt;
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt -o socks4.txt;
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -o http.txt;
+
