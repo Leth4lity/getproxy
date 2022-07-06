f6b1720dad97691b003fc28ca7ec7d29e8adc2d9
Laken J Brion <60785066+Leth4lity@users.noreply.github.com>
2022-07-06 07:55:22 -0500

    Changed output file names.
    Added "-proxys.txt"

--- getproxy.sh
+++ getproxy.sh
@@ -1,4 +1,4 @@
-curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt -o socks5.txt;
-curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt -o socks4.txt;
-curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -o http.txt;
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt -o socks5-proxys.txt;
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt -o socks4-proxys.txt;
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -o http-proxys.txt;
 
